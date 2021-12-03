7ab835cc59ac481968500110b27cf56a7358c0d6
J. Famous <joshua.famous@gmail.com>
2021-12-02 20:50:58 -0500

    Finish create player

---
+++ createPlayer.php
@@ -0,0 +1,66 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$conf = include('config.php');
+
+$username = $conf['username'];
+$password = $conf['password'];
+$host = $conf['host'];
+$dbname = $conf['dbname'];
+$port = $conf['port'];
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>FootballSimXYZ</title>
+    </head>
+    <body>
+		<p>
+			<?php 
+				echo "Creating new player: " . $_POST["fname"] . " " . $_POST["lname"] . "..."; 
+				try {
+					// Create PDO
+					$conn = new PDO("mysql:host=$host;dbname=$dbname;port=$port", $username, $password);
+					$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+					// Start transaction
+					$conn->beginTransaction();
+					// Attempt create player
+					$sql1 = "INSERT INTO players (fname, mname, lname, age) VALUES (?,?,?,?)";
+					$st1 = $conn->prepare($sql1);
+					$st1->execute(array($_POST["fname"], $_POST["mname"], $_POST["lname"], $_POST["age"]));
+					$playerid = $conn->lastInsertId();
+					// Attempt to add team
+					$sql2 = "INSERT INTO playerfor (playerid, teamid, fromdate) VALUES (?, ?, NOW())";
+					$st2 = $conn->prepare($sql2);
+					$st2->execute(array($playerid, $_POST["team"]));
+					// Attempt to add position
+					$sql3 = "INSERT INTO playsposition (playerid, posid) VALUES (?, ?)";
+					$st3 = $conn->prepare($sql3);
+					$st3->execute(array($playerid, $_POST["position"]));
+					// Commit transaction
+					$conn->commit();
+					echo "Successfully created new player";
+			?>
+				<p>You will be redirected in 2 seconds</p>
+				<script>
+					var timer = setTimeout(function() {
+						window.location='players.php'
+					}, 2000);
+				</script>
+			<?php
+				} catch(PDOException $e) {
+					// Rollback on failure
+					$conn->rollBack();
+					echo $sql . "<br>" . $e->getMessage();
+					echo '<a id="backButton" href="./home.php">Home</a>';
+				}
+				$conn = null;
+			?>
+		</p>
+    </body>
+</div>
+</html>
--- players.php
+++ players.php
@@ -17,6 +17,12 @@ try {
     $playersQuery = 'SELECT P.playerid, P.fname, P.mname, P.lname, PO.abbr, P.age FROM players P INNER JOIN playsposition PP ON PP.playerid=P.playerid INNER JOIN positions PO ON PO.posid=PP.posid';
     $playersResults = $conn->query($playersQuery);
     $playersResults->setFetchMode(PDO::FETCH_ASSOC);
+    $positionsQuery = 'SELECT * FROM positions';
+    $positionsResults = $conn->query($positionsQuery);
+    $positionsResults->setFetchMode(PDO::FETCH_ASSOC);
+    $teamsQuery = 'SELECT * FROM teams ORDER BY city';
+    $teamsResults = $conn->query($teamsQuery);
+    $teamsResults->setFetchMode(PDO::FETCH_ASSOC);
 } catch (PDOException $e) {
     die("Could not connect to the database $dbname :" . $e->getMessage());
 }
@@ -39,7 +45,7 @@ try {
         <a id="backButton" href="./home.php">Home</a>
         <h1 id="mainTitle">Players</h1>
         <div class="row">
-            <div class="col50">
+            <div class="col50" style="overflow-y: scroll;">
                 <h4 class="subTitle">Current Players</h4>
                 <div class="dataTable">
                     <div class="headerRow">
@@ -64,6 +70,30 @@ try {
             </div>
             <div class="col50">
                 <h4 class="subTitle">Create a Player</h4>
+                <form action="/createPlayer.php" method="post" class="dataForm">
+                    <p class="formLabel">First Name</p>
+                    <input class="dataInput" type="text" id="fname" minlength="1" name="fname" value="" required>
+                    <p class="formLabel">Middle Name</p>
+                    <input class="dataInput" type="text" id="mname" name="mname" value="" required>
+                    <p class="formLabel">Last Name</p>
+                    <input class="dataInput" type="text" id="lname" minlength="1" name="lname" value="" required>
+                    <p class="formLabel">Age</p>
+                    <input class="dataInput" type="number" min="18" max="60" step="1" id="age" name="age" value="20" required>
+                    <p class="formLabel">Position</p>
+                    <select class="dataInput" name="position" id="position" required>
+                        <?php while($position = $positionsResults->fetch()) : ?>
+                            <option value="<?php echo htmlspecialchars($position['posid'])?>"><?php echo htmlspecialchars($position['name'])?></option>
+                        <?php endwhile; ?>
+                    </select>
+                    <p class="formLabel">Team</p>
+                    <select class="dataInput" name="team" id="team" required>
+                        <?php while($team = $teamsResults->fetch()) : ?>
+                            <option value="<?php echo htmlspecialchars($team['teamid'])?>"><?php echo htmlspecialchars($team['city']) . " " . htmlspecialchars($team['name'])?></option>
+                        <?php endwhile; ?>
+                    </select>
+                    <br>
+                    <input class="formButton" type="submit" value="Create">
+                </form>
             </div>
         </div>
     </body>
